dbd3daf3851433abf9dc2f6db621ec90e7db5ea0
Patrick R. Wan <527672920@qq.com>
2015-09-01 10:54:36 +0800

    Create ColorDistribution.m

---
+++ ColorDistribution.m
@@ -0,0 +1,25 @@
+clear all
+close all
+I=imread('reflection.jpg');
+I = imresize(I,0.16);
+s=size(I,1)*size(I,2);
+scale_down=0.95; % we want to plot only 5% color
+RANDperm = randperm(s); % randomly choosing those 5% colors
+R=double(reshape(I(:,:,1),1,s));
+G=double(reshape(I(:,:,2),1,s));
+B=double(reshape(I(:,:,3),1,s));
+
+R(RANDperm(1:(s*scale_down))) = NaN; 
+G(RANDperm(1:(s*scale_down))) = NaN; 
+B(RANDperm(1:(s*scale_down))) = NaN; 
+R(isnan(R))=[];% deleting the NaN entries
+G(isnan(G))=[];% deleting the NaN entries
+B(isnan(B))=[];% deleting the NaN entries
+
+% This linearly assigns color from colormap into data but does not have RGB significance
+figure;imshow(I);
+figure;scatter3(R,G,B,30,[R' G' B']./255,'filled','Marker','o');
+xlabel('RED');ylabel('GREEN');zlabel('BLUE');
+title('RGB values corresponding to real values');
+
+% Let's plot data which are corresponding to their real RGB values 
